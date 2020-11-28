4b99101d513e3d3478989d62599cb7583153cc0f
Manuhiri ANIHIA <lovanima@hotmail.com>
2020-11-28 14:55:35 +0100

    mise à jours du triangle de pascale

--- Game1/pascal.js
+++ Game1/pascal.js
@@ -1,11 +1,32 @@
 // += signifie concaténer
 
 
+//Je créer un booléen pour éviter d'afficher plusieurs fois le résultat lorsque l'on clique sur valider
+var bool = true;
+
+
 //Je récupère la valeur saisie par l'internaute via l'évènement click
 document.getElementById("valider").addEventListener("click", function () {
-    // Je récupère la value de l'élément qui a pour id info
-    var taille = document.getElementById("infos").value;
-    afficherTriangle(trianglePascale(taille));
+
+    //Le if nous permet de ne pas réaffciher le résultat plusieru fois
+    if (bool == true) {
+        // Je récupère la value de l'élément qui a pour id info
+        var taille = document.getElementById("infos").value;
+        afficherTriangle(trianglePascale(taille));
+        bool = false;
+    };
+    
+});
+
+//Je récupère la valeur saisie par l'internaute via l'évènement click
+document.getElementById("valider-1").addEventListener("click", function () {
+
+    //Le if nous permet de ne pas réaffciher le résultat plusieru fois
+    if (bool == true) {
+        // Je récupère la value de l'élément qui a pour id info
+        var numero = document.getElementById("stern").value;
+        afficherStern(stern(numero));
+    };
 });
 
 
@@ -45,6 +66,7 @@ console.log(trianglePascale(3));
 /////////////////////////////////////////////////////////////////////////
 
 
+//afficher le triangle de pascale
 function afficherTriangle(tab) {
     // Je balais le premier tableau
     for (var i = 0; i < tab.length; i++) {
@@ -57,3 +79,64 @@ function afficherTriangle(tab) {
 
     };
 };
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+// Je créer la fonction de stern
+function stern(numero) {
+    //On génère un triangle de pascale qui nous servira pour créer la suite de stern
+    var tab1 = trianglePascale(numero+100)
+    var tab2 = [];
+    //Je renseigne les 4 premier élément de la suite de stern
+    tab2.push(1);
+    tab2.push(1);
+    tab2.push(2);
+    tab2.push(1);
+    //création de la boucle
+    for (var i = 4; i < numero; i++) {
+
+        var sum = 0; 
+        var newVal = Math.trunc(i/2);
+        for (j = 0; j < newVal+1; j++) {
+            sum += tab1[i-j][j]%2;
+        };
+
+        tab2.push(sum);
+    };
+
+    return tab2;
+};
+
+console.log(stern(7));
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+//Affcher le suite de stern
+function afficherStern(tab) {
+    for (var i = 0; i < tab.length; i++) {
+        var u = i + 1;
+        document.getElementById("stern-res").innerHTML += "s" + u + "=" + " " + tab[i] + " ";
+    };
+};
+
+
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+
+//Activer les boutons reset
+document.getElementById("retry").addEventListener("click", function () {
+    //fonction reload pour recharger la page prend un boolean en paramètre
+    document.location.reload(true);
+});
+
+document.getElementById("retry-1").addEventListener("click", function () {
+    //fonction reload pour recharger la page prend un boolean en paramètre
+    document.location.reload(true);
+});
